bb3ca7451d8d0ab78a1835e56e08688a42b5c609
Federico Zappone <kingzappo@protonmail.com>
2022-12-30 01:24:57 +0100

    switched to pylsp

--- plugins/init.lua
+++ plugins/init.lua
@@ -29,7 +29,7 @@ return {
         "codelldb",
 
         -- python
-        "pyright",
+        "python-lsp-server",
         "black",
       },
     },
--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -3,7 +3,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "clangd", "pyright" }
+local servers = { "clangd", "pylsp" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
