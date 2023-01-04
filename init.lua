d76826a986074a22d6906fd2fee1a5a316877525
Federico Zappone <kingzappo@protonmail.com>
2023-01-04 01:12:56 +0100

    small changes

--- init.lua
+++ init.lua
@@ -2,3 +2,8 @@ local opt = vim.opt
 
 opt.scrolloff=8
 opt.sidescrolloff=8
+
+-- hilight yank
+vim.api.nvim_exec([[
+au TextYankPost * lua vim.highlight.on_yank {higroup="IncSearch", timeout=150, on_visual=true}
+]],false)
